8671db65c7a40e4760ce8f74f46f7aae5def3e4a
Valerii Minko <vminko@hrk.langate.internal>
2020-05-20 16:24:04 +0300

    Updates

---
+++ precache-manifest.b468b7c1d5eb04510d13c03609489b58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da7956c4d395f7acc04c270f51b78abd",
+    "url": "/formapp/index.html"
+  },
+  {
+    "revision": "73b4776b7886e96cb05c",
+    "url": "/formapp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d292eabbfa18d1747874",
+    "url": "/formapp/static/css/main.aec6d9da.chunk.css"
+  },
+  {
+    "revision": "73b4776b7886e96cb05c",
+    "url": "/formapp/static/js/2.a66dee99.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/formapp/static/js/2.a66dee99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d292eabbfa18d1747874",
+    "url": "/formapp/static/js/main.fda72b55.chunk.js"
+  },
+  {
+    "revision": "f2e9280628e188a09523",
+    "url": "/formapp/static/js/runtime-main.60dc016e.js"
+  }
+]);
